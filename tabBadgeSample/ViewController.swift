ccbbf8a7ba672dcf9d93245e163e4e500647bbc3
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-05-08 17:11:49 +0900

    Initial Commit

---
+++ tabBadgeSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabBadgeSample
+//
+//  Created by satoshi.marumoto on 2020/05/08.
+//  Copyright © 2020 satoshi.marumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
